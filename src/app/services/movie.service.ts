e674984fe1e62f8f097e451777637ee4318007a5
Lara Souza <laragabriellysouzabatista@gmail.com>
2022-02-02 13:53:57 -0300

    changes

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,51 +1,52 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ApiKey, BaseUrl } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
+   ApiKey ="5330ecf323f9caf08ceb1326b924d61c"
+   BaseUrl ="https://api.themoviedb.org/3"
+  
   constructor(
   private http: HttpClient
   ) {}
 
   getMovies(type: string, page: number): Observable<any> {
-    return this.http.get(`${BaseUrl}movie/${type}?api_key=${ApiKey}&page=${page}`)
+    return this.http.get(`${this.BaseUrl}movie/${type}?api_key=${this.ApiKey}&page=${page}`)
   }
   getPopularMovies(): Observable<any> {
-    return this.http.get(`${BaseUrl}/movie/popular?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/movie/popular?api_key=${this.ApiKey}`)
   }
   getUpcomingMovies(): Observable<any>{
-    return this.http.get(`${BaseUrl}/movie/upcoming?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/movie/upcoming?api_key=${this.ApiKey}`)
   }  
   getTopRatedMovies(): Observable<any>{
-    return this.http.get(`${BaseUrl}/movie/top_rated?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/movie/top_rated?api_key=${this.ApiKey}`)
   }
   getNowPlayingMovies(): Observable<any>{
-    return this.http.get(`${BaseUrl}/movie/now_playing?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/movie/now_playing?api_key=${this.ApiKey}`)
   }
   getMovieGenres(): Observable<any>{
-    return this.http.get(`${BaseUrl}/genre/movie/list?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/genre/movie/list?api_key=${this.ApiKey}`)
   }
   getMoviesByGenre(id:number): Observable<any> {
-    return this.http.get(`${BaseUrl}/genre/${id}/movies?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/genre/${id}/movies?api_key=${this.ApiKey}`)
   }
   getMovie(id: number): Observable<any> {
-    return this.http.get(`${BaseUrl}/movie/${id}?api_key=${ApiKey}&append_to_response=videos,images,details,reviews,similar`)
+    return this.http.get(`${this.BaseUrl}/movie/${id}?api_key=${this.ApiKey}&append_to_response=videos,images,details,reviews,similar`)
   }
   
   getSimilarMovies(id: number): Observable<any> {
-    return this.http.get(`${BaseUrl}/movie/${id}/similar?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/movie/${id}/similar?api_key=${this.ApiKey}`)
   }
 
   searchMovies(name:string):Observable<any>{
-    return this.http.get(`${BaseUrl}/search/multi?api_key=${ApiKey}&query=${name}`)
+    return this.http.get(`${this.BaseUrl}/search/multi?api_key=${this.ApiKey}&query=${name}`)
   }
 
   getAllMovies(page: number):Observable<any>{
-  return this.http.get(`${BaseUrl}/movie/now_playing?api_key=${ApiKey}&language=en-US&page=${page}`)
+  return this.http.get(`${this.BaseUrl}/movie/now_playing?api_key=${this.ApiKey}&language=en-US&page=${page}`)
   }
 }
--- src/app/services/tv-shows.service.ts
+++ src/app/services/tv-shows.service.ts
@@ -1,38 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ApiKey, BaseUrl } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TvShowsService {
-
+  ApiKey ="5330ecf323f9caf08ceb1326b924d61c"
+  BaseUrl ="https://api.themoviedb.org/3"
   constructor(
     private http: HttpClient
   ) { }
   getPopularTvShows(): Observable<any> {
-    return this.http.get(`${BaseUrl}/tv/popular?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/tv/popular?api_key=${this.ApiKey}`)
   }
   getOnTheAirTvShows(): Observable<any>{
-    return this.http.get(`${BaseUrl}/tv/on_the_air?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/tv/on_the_air?api_key=${this.ApiKey}`)
   }  
   getTopRatedTvShows(): Observable<any>{
-    return this.http.get(`${BaseUrl}/tv/top_rated?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/tv/top_rated?api_key=${this.ApiKey}`)
   }
   getLatestTvShows(): Observable<any>{
-    return this.http.get(`${BaseUrl}/tv/latest?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/tv/latest?api_key=${this.ApiKey}`)
   }
   getTvShowsDetails(id:number): Observable<any>{
-    return this.http.get(`${BaseUrl}/tv/${id}?api_key=${ApiKey}&append_to_response=videos,images,details`)
+    return this.http.get(`${this.BaseUrl}/tv/${id}?api_key=${this.ApiKey}&append_to_response=videos,images,details`)
   }
   getSimilarTvShow(id:number): Observable<any>{
-    return this.http.get(`${BaseUrl}/tv/${id}/similar?api_key=${ApiKey}&append_to_response=videos,images,details`)
+    return this.http.get(`${this.BaseUrl}/tv/${id}/similar?api_key=${this.ApiKey}&append_to_response=videos,images,details`)
   }
   getAllTvShows(page:number): Observable<any>{
-    return this.http.get(`${BaseUrl}/tv/popular?api_key=${ApiKey}&language=en-US&page=${page}`)
+    return this.http.get(`${this.BaseUrl}/tv/popular?api_key=${this.ApiKey}&language=en-US&page=${page}`)
   }
   getTvShowGenres():Observable<any>{
-    return this.http.get(`${BaseUrl}/genre/tv/list?api_key=${ApiKey}`)
+    return this.http.get(`${this.BaseUrl}/genre/tv/list?api_key=${this.ApiKey}`)
   }
 }
